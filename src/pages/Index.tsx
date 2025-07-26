875d71b05af7f3430885f189932b031751c65fd6
swapndeeptomar <138565087+swapndeeptomar@users.noreply.github.com>
2025-07-26 23:34:07 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -98,7 +98,7 @@ const Index = () => {
       <Navbar />
       
     
-      <section className="w-full relative h-[92vh] md:h-[92vh]">
+      <section className="w-full relative h-[93vh] md:h-[93vh]">
         <div className="w-full h-full">
           <video
             ref={videoRef}
